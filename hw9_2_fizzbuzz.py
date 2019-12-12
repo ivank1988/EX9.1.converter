7cfa875af45aa4675b02c0404d2411a383b77241
Ivan Kralj <ivan.kralj0511@gmail.com>
2019-12-12 18:24:17 +0100

    update

--- hw9_2_fizzbuzz.py
+++ hw9_2_fizzbuzz.py
@@ -1,15 +1,11 @@
+end = int(input("Please enter a number between 1 and 100: "))
 
-print("Welcome to the fizzbuzz game!")
-
-end = input("Please enter a number between 1 and 100: ")
-end = int(end)  # convert string into number
-
-for num in range(1, end+1):
-    if num % 3 == 0 and num % 5 == 0:
+for i in range(1, end+1):
+    if i % 3 == 0 and i % 5 == 0:
         print("fizzbuzz")
-    elif num % 3 == 0:
+    elif i % 3 == 0:
         print("fizz")
-    elif num % 5 == 0:
+    elif i % 5 == 0:
         print("buzz")
     else:
-        print(num)
+        print(i)
