666c514ec66663d2a586b12c5d480066366e0d3d
Ivan Kralj <ivan.kralj0511@gmail.com>
2019-12-12 19:18:55 +0100

    initial commit

--- coverterKMtoMILES.py
+++ coverterKMtoMILES.py
@@ -6,9 +6,11 @@ mile = 1.61 * kilometer
 
 print (str(kilometer) + " kilometers = " + str(mile) + " miles")
 
+
 upit = int(input("do you want another try (yes=1/no=0): "))
 
 while upit == 1:
+
     kilometer = int(input("unesi broj km: "))
     print(str(kilometer) + " kilometers = " + str(mile) + " miles")
 
