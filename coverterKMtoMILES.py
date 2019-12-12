3b3469ebe2ac408db6161c093cfecfc9807d1c94
Ivan Kralj <ivan.kralj0511@gmail.com>
2019-12-12 20:01:51 +0100

    sve sam pomalno uredio

--- coverterKMtoMILES.py
+++ coverterKMtoMILES.py
@@ -1,19 +1,18 @@
 print("This is converter between km and miles")
 
-kilometer = int(input("unesi broj km: "))
 
-mile = 1.61 * kilometer
-
-print (str(kilometer) + " kilometers = " + str(mile) + " miles")
-
-
-upit = int(input("do you want another try (yes=1/no=0): "))
-
-while upit == 1:
+while True:
 
     kilometer = int(input("unesi broj km: "))
+
+    mile = 1.61 * kilometer
+
     print(str(kilometer) + " kilometers = " + str(mile) + " miles")
 
+    upit = int(input("do you want another try (yes=1/no=0): "))
+
+    if upit == 0:
+        break
 else:
     print ("goodbye")
 
